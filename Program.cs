a1e7d704fe959ccec2a868bb4686e054f4701bea
Jessica Janine de Oliveira <84152746+JessicaJanine@users.noreply.github.com>
2022-02-18 13:50:01 -0300

    Curso - Pilares POO
    Arquivos dos exemplos de Abstração e Encapsulamento.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PilaresPOO.Models;
+using System;
+
+namespace PilaresPOO
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+            p1.Nome = "Jarvis";
+            p1.Idade = 50;
+
+            p1.Apresentar();
+
+            Retangulo r = new Retangulo();
+            r.DefinirMedidas(25, 50);
+            System.Console.WriteLine($"Área: {r.ObterArea()}");
+
+            
+        }
+    }
+    
+}
+
+
+
