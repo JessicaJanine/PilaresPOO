558429f6a19c8095835a2bd79617c42283d64426
Jessica Janine de Oliveira <84152746+JessicaJanine@users.noreply.github.com>
2022-04-11 21:37:39 -0300

    Atualização com Interfaces
    Conceito de interface e exemplo de código com uma interface ICalculadora e a sua implementação na classe Calculadora.

--- Calculadora.cs
+++ Calculadora.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PilaresPOO.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,18 @@ using System.Threading.Tasks;
 
 namespace PilaresPOO.Models
 {
-    public class Calculadora
+    public class Calculadora : ICalculadora
     {
+        public int Dividir(int n1, int n2)
+        {
+            return n1 / n2;
+        }
+
+        public int Multiplicar(int n1, int n2)
+        {
+            return n1 * n2;
+        }
+
         public int Somar (int num1, int num2)
         {
             return num1 + num2;
@@ -16,6 +27,11 @@ namespace PilaresPOO.Models
         public int Somar (int num1, int num2, int num3)
         {
             return num1 + num2 + num3;
+        }
+
+        public int Subtrair(int n1, int n2)
+        {
+            return n1 - n2;
         }
     }
 }
---
+++ ICalculadora.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PilaresPOO.Interfaces
+{
+    public interface ICalculadora
+    {
+        int Somar(int n1, int n2);
+        int Subtrair(int n1, int n2);
+        int Multiplicar(int n1, int n2);
+        int Dividir(int n1, int n2)
+        {
+            return n1 / n2;
+        }
+        //Um método implementado na interdace pode não ser implementado na classe e puxado direto da interface.
+        //Os demais métodos não implementados aqui tem que ser implementados na classe obrigatoriamente.
+    }
+}
